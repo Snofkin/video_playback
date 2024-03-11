a10a34503296fa08835fcc6cd608a2be508d448d
amirsuwal <suwala01@gmail.com>
2024-03-11 16:16:57 +0545

    remove unnecessary code block

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'core/injection/injection.dart';
 import 'features/video/presentation/pages/video_page.dart';
@@ -15,17 +14,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-    return Shortcuts(
-        shortcuts: <LogicalKeySet, Intent>{
-          LogicalKeySet(LogicalKeyboardKey.select): const ActivateIntent(),
-        },
-        child: MaterialApp(
-          title: 'Video Player',
-          theme: ThemeData(
-            primarySwatch: Colors.green,
-          ),
-          home: VideoPage(),
-        ));
+    return MaterialApp(
+      title: 'Video Player',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: VideoPage(),
+    );
   }
 }
